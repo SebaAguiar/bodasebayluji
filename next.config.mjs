003f9bc3163512333b0264d75658b8740213bbe1
SebaAguiar <sebaaguiar08@gmail.com>
2024-02-05 12:31:28 -0300

    headers changes

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,26 @@ const nextConfig = {
     }
     return config;
   },
+  async headers() {
+    return [
+      {
+        // Aplicando a todas las rutas de la API
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
 };
 
